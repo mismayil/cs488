53f1080ed7433cb1c71f6d0626fe66f37498b58b
Mahammad Ismayilzada <gmahammad12@gmail.com>
2015-09-20 23:53:57 -0400

    add source files

---
+++ shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/context.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/glx_context.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/init.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/input.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/linux_joystick.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/monitor.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/posix_time.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/posix_tls.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/posix_tls.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/window.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/x11_init.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/x11_monitor.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/x11_window.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/src/xkb_unicode.c" "/home/mismayil/UW/CS488/dev/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
